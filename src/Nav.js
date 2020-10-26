7ce1b3f499593d0382f90ff35e2dd87c1f098b87
Sachwini <sachwinikhatri@gmail.com>
2020-10-26 13:34:17 +0545

    my first commit

--- src/Nav.js
+++ src/Nav.js
@@ -18,13 +18,13 @@ function Nav() {
         <div className={`nav ${show && "nav_black"}`}>
             <img
                 className="nav_logo"
-                src="https://upload.wikimedia.org.wikipedia/commons/0/0f/"
+                src="https://upload.wikimedia.org/wikipedia/commons/0/0f/Logo_Netflix.png"
                 alt="Netflix Logo"           
             />
 
             <img
                 className="nav_avatar"
-                src="https://pbs.twimg.com.profile_images/124011999041155"
+                src="https://pbs.twimg.com/profile_images/1240119990411550720/hBEe3tdn_400x400.png"
                 alt="Netflix Logo"  
             />
             
